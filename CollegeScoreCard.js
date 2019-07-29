565d13fa1701a06fd83774e2bf63e389c1be623a
Alexander H Lee III <ahlee1026@gmail.com>
2019-07-29 01:58:37 -0400

    fixed the button and filters union

--- CollegeScoreCard.js
+++ CollegeScoreCard.js
@@ -2,26 +2,107 @@ function cscard() {
         
   var cityn = "atlanta";
   // var subject = "mathematics";
-  var queryURL = `https://api.data.gov/ed/collegescorecard/v1/schools/nearest.json?api_key=HNg7UV31G0EyV08Fh2zkZBi1WNM1XffC3ttbfbw5&school.city=${cityn}&_per_page=100`;
-
+  var queryURL = `https://api.data.gov/ed/collegescorecard/v1/schools/nearest.json?api_key=HNg7UV31G0EyV08Fh2zkZBi1WNM1XffC3ttbfbw5&school.state=ga&_per_page=100`;
+  
   // var queryURL = `https://api.data.gov/ed/collegescorecard/v1/schools/nearest.json?api_key=HNg7UV31G0EyV08Fh2zkZBi1WNM1XffC3ttbfbw5&school.city=${school}&_per_page=100`;
 
 
-
+  
 var filteredSchools = [];
 // function getResults() {
-// //From Jack: get all the form input from our quiz
-  // var subjectMath = ("#mathBtn").val();
-  // var subjectScience = ("#scienceBtn").val();
-  // var subjectSStudies = ("#sStudiesBtn").val();
-  // var subjectLiberalArts = ("#lArtsBtn").val();
-var mensSchool = $("#allMaleBtn").val(); //From Jack: the jquery won't look exactly like this but this should evaluate to T/F
-var femaleSchool = $("#allFemaleBtn").val();
-// var religiousYes = $("#q3YesBtn").val();
-var retentionRatesYes = $("#q5YesBtn").val();
-var averageCostYes = $("#q6YesBtn").val();
-// //need to work on question 7 buttons/id's
-var aidYes = $("#q8YesBtn").val();
+  //From Jack: get all the form input from our quiz
+  if ($('#mathBtn').is(":checked"))
+  {
+    // it is checked
+    var subjectMath = true;
+    console.log("math:: "+subjectMath);
+  }
+  if ($('#scienceBtn').is(":checked"))
+  {
+    // it is checked
+    var subjectScience = true;
+    console.log("sciene:: "+subjectScience);
+  }
+
+  if ($('#sStudiesBtn').is(":checked"))
+  {
+    // it is checked
+    var subjectSStudies = true;
+    console.log("socislsudies:: "+subjectSStudies);
+  }
+
+  if ($('#lArtsBtn').is(":checked"))
+  {
+    // it is checked
+    var subjectLiberalArts = true;
+    console.log("liberalArt:: "+subjectLiberalArts);
+  }
+
+if ($('#allMaleBtn').is(":checked"))
+{
+  // it is checked
+  var mensSchool = true; //From Jack: the jquery won't look exactly like this but this should evaluate to T/F
+  console.log("mens:: "+mensSchool);
+}
+
+if ($('#allFemaleBtn').is(":checked"))
+{
+  // it is checked
+  // var femaleSchool = $("#allFemaleBtn").val();
+  var femaleSchool = true;
+  console.log("Femms:: "+femaleSchool);
+}
+
+if ($('#q3YesBtn').is(":checked"))
+{
+  // it is checked
+  var religiousYes = true;
+  console.log("rel:: "+religiousYes);
+}
+
+if ($('#q5YesBtn').is(":checked"))
+{
+  // it is checked
+  var retentionRatesYes = true;
+  console.log("ret:: "+retentionRatesYes);
+}
+
+if ($('#q6YesBtn').is(":checked"))
+{
+  // it is checked
+  var avgerageTuitionYes = true;
+  console.log("tui:: "+avgerageTuitionYes);
+}
+
+if ($('#q7YesBtn1').is(":checked"))
+{
+  // it is checked
+  var averageCostYes1 = true;
+  console.log("avgC:: "+averageCostYes1);
+}
+if ($('#q7YesBtn2').is(":checked"))
+{
+  // it is checked
+  var averageCostYes2 = true;
+  console.log("avgC:: "+averageCostYes2);
+}
+if ($('#q7YesBtn3').is(":checked"))
+{
+  // it is checked
+  var averageCostYes3 = true;
+  console.log("avgC:: "+averageCostYes3);
+}
+
+if ($('#q8YesBtn').is(":checked"))
+{
+  // it is checked
+  var aidYes = true;
+  console.log("aid:: "+aidYes);
+}
+
+
+
+  // //need to work on question 7 buttons/id's
 
     // var subjectMath = true;
     // var subjectScience = true;
@@ -53,6 +134,7 @@ meta: {
   response.results.forEach(function(schoolObject) {
   //   console.log(schoolObject);
     var schooli = schoolObject["2017"];
+    //console.log(schoolObject);
     var schoolIsValid = true;
     
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -90,23 +172,23 @@ meta: {
 
     if (mensSchool === true ) {
         //[""0""].school.men_only
-      if (schoolObject.school.women_only !== 0 ) {
+      if (schoolObject.school.women_only === 1 ) {
         schoolIsValid = false;
       }
     }
 
     if (femaleSchool === true ) {
-      if (schoolObject.school.men_only !== 0 ) {
+      if (schoolObject.school.men_only === 1 ) {
         schoolIsValid = false;
       }
     }
-    // if (religiousYes === true ) {
-    //     //[2].school.religious_affiliation
-    //   if (schoolObject.school.religious_affiliation = null  ) {
-    //     schoolIsValid = false;
-    //   }
+    if (religiousYes === true ) {
+        //[2].school.religious_affiliation
+      if (schoolObject.school.religious_affiliation === null  ) {
+        schoolIsValid = false;
+      }
       
-    // }
+    }
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // //need correct name to be pulled from data dictionary
     if (retentionRatesYes === true ) {
@@ -116,19 +198,19 @@ meta: {
       }
     }
 // //not sure if this one is correct
-    if (averageCostYes === 1 ) {
+    if (averageCostYes1 === true ) {
       //[2][2017].cost.attendance.academic_year
       if (schooli.cost.attendance.academic_year > 10000 ) {
         schoolIsValid = false;
       }
     }
-    if (averageCostYes === 2 ) {
+    if (averageCostYes2 === true ) {
       //[2][2017].cost.attendance.academic_year
       if (schooli.cost.attendance.academic_year > 30000 ) {
         schoolIsValid = false;
       }
     }
-    if (averageCostYes === 3 ) {
+    if (averageCostYes3 === true ) {
       //[2][2017].cost.attendance.academic_year
       if (schooli.cost.attendance.academic_year < 30000 ) {
         schoolIsValid = false;
@@ -149,7 +231,7 @@ meta: {
       console.log("this is valid:: "+schoolObject.school.name);
       // console.log("this is valid:: "+response.results[i].school.name);
       filteredSchools.push(schoolObject);
-      console.log(filteredSchools);
+      // console.log(filteredSchools);
     }
 
   })
